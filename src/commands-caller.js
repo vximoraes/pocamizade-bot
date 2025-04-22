8afbb046558c4fb0f1cbee189b193dc738571cad
Eduardo Tartas <eduardos.tartas@gmail.com>
2025-04-22 14:57:38 -0400

    feat: add command handling for 'ping' and 'chamar' interactions

---
+++ src/commands-caller.js
@@ -0,0 +1,18 @@
+import { client } from "./index.js";
+
+ export async function callCommands (){
+    client.on('interactionCreate', (interaction) => {
+        // Verifica se a interação é um comando
+        if(!interaction.isChatInputCommand) return;
+        console.log('Comando recebido:', interaction.commandName);
+        // Verifica se o comando é o "ping"
+        if(interaction.commandName === 'ping') {
+          interaction.reply('Pong!');
+        }
+        // Verifica se o comando é o "chamar"
+        if(interaction.commandName === 'chamar') {
+          interaction.reply('Chamando viados! @everyone');
+        }
+        
+      });
+}
