4bcb92e3543ebd8f0fc0c869fb715a1674aa784f
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-25 13:26:50 -0400

    feat: implement getWeather function to fetch current weather data

---
+++ src/services/weatherApi.js
@@ -0,0 +1,24 @@
+import { config } from '../config/dotenv.js';
+
+const api = {
+    key: config.weatherApiKey,
+    base: "https://api.weatherapi.com/v1"
+};
+
+export async function getWeather(city) {
+    console.log('Cidade recebida pela API:', city);
+    return fetch(`${api.base}/current.json?key=${api.key}&q=${city}&aqi=no`)
+        .then(response => response.json())
+        .then(data => {
+            const weather = {
+                temperature: data.current.temp_c,
+                feelsLike: data.current.feelslike_c,
+                last_updated: data.current.last_updated,
+                condition: data.current.condition.text,
+                icon: data.current.condition.icon,
+                region: data.location.region,
+            };
+            return weather;
+        })
+        .catch(error => console.error('Error on fetching', error));
+}
