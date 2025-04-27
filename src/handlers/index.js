509df6522534ea5ae66bb99127ee0effcbe09489
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-27 00:36:08 -0400

    feat: create index.js to export clima and rickroll handlers

---
+++ src/handlers/index.js
@@ -0,0 +1,7 @@
+import { climaHandler } from "./climaHandler.js";
+import { rickrollHandler } from "./rickrollHandler.js";
+
+export const handlers = {
+    clima: climaHandler,
+    rickroll: rickrollHandler,
+};
