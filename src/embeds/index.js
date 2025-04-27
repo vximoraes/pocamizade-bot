978b711072df086c4acfb9da1d0613f69c8c5755
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-27 00:35:21 -0400

    feat: create index.js to export weather and rickroll embeds

---
+++ src/embeds/index.js
@@ -0,0 +1,7 @@
+import { weatherEmbed } from "./climaEmbed.js";
+import { rickrollEmbed } from "./rickrollEmbed.js";
+
+export const embeds = {
+    clima: weatherEmbed,
+    rickroll: rickrollEmbed,
+};
